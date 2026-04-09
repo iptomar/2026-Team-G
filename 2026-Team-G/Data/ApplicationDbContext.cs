3b8190cdccc429aebe41a0b5d5ccd4319dbdfbb3
João Abreu <jaabrel@outlook.pt>
2026-04-09 18:42:00 +0100

    Commit Inicial da Criação do Projeto em .Net 10.0

---
+++ 2026-Team-G/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace _2026_Team_G.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
