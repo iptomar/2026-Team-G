2fda6d6ea63b89261abf2ae6393253fa95eb46c4
Rodrigo Pinheiro <aluno26402@ipt.pt>
2026-05-14 18:00:28 +0100

    Registo e login v2

--- 2026-Team-G/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ 2026-Team-G/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -76,6 +76,10 @@ namespace _2026_Team_G.Areas.Identity.Pages.Account
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
             [Required]
+            [Display(Name = "Nome de Utilizador")]
+            public string Username { get; set; }
+
+            [Required]
             [EmailAddress]
             [Display(Name = "Email")]
             public string Email { get; set; }
@@ -115,7 +119,7 @@ namespace _2026_Team_G.Areas.Identity.Pages.Account
             {
                 var user = CreateUser();
 
-                await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
+                await _userStore.SetUserNameAsync(user, Input.Username, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
